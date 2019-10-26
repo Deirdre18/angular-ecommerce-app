b6179562e08935450720659bae5354906ef933a4
Deirdre18 <deirdreweldon18@gmail.com>
2019-10-26 18:55:07 +0100

    making notification button work - modifying product-alerts.component.ts

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -4,7 +4,8 @@ import { Input } from '@angular/core';
 /*@Component() decorator indicates the following class is a component. It provides metadata about the component, including its selector, templates, and styles*/
 
 
-
+/*the product alert component is enabled to emit an event when the user clicks "Notify Me"*/
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   /*The selector identifies the component and is the name given to the Angular component when it is rendered as an HTML element on the page. By convention, Angular component selectors begin with the prefix app- followed by the component name*/
@@ -14,8 +15,11 @@ import { Input } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 /*ProductAlertsComponent handles functionality for the component*/
+
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
+  /*defining a property named notify with an @Output() decorator and an instance of EventEmitter(). This allows the product alert component to emit an event when the value of the notify property changes*/
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,11 +7,17 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent {
   products = products;
 
   share() {
     window.alert('The product has been shared!');
+  }
+/*defining the behavior that should happen when the user clicks the button. It's the parent (product list component), not the product alerts component that acts when the child raises the event. Therefore defining an onNotify() method, similar to the share() method*/
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
